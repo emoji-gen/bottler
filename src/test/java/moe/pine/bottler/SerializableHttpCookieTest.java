2b965934e2201bef907eae11c35e0f1794bb9017
Pine Mizune <pinemz@gmail.com>
2017-04-08 16:10:36 +0900

    test(SerializableHttpCookie): add tests

--- src/test/java/moe/pine/bottler/SerializableHttpCookieTest.java
+++ src/test/java/moe/pine/bottler/SerializableHttpCookieTest.java
@@ -1,6 +1,7 @@
 package moe.pine.bottler;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 import java.net.HttpCookie;
 
@@ -9,6 +10,7 @@ import java.net.HttpCookie;
  * Created by pine on 2017/04/08
  */
 public class SerializableHttpCookieTest {
+    @Test
     public void testOf() {
         HttpCookie cookie = new HttpCookie("name", "value");
         cookie.setComment("comment");
@@ -19,7 +21,7 @@ public class SerializableHttpCookieTest {
         cookie.setPortlist("443");
         cookie.setSecure(true);
         cookie.setHttpOnly(true);
-        cookie.setVersion(10);
+        cookie.setVersion(1);
 
         SerializableHttpCookie newCookie = SerializableHttpCookie.of(cookie);
         Assert.assertEquals(cookie.getName(), newCookie.getName());
@@ -31,5 +33,59 @@ public class SerializableHttpCookieTest {
         Assert.assertEquals(cookie.getSecure(), newCookie.getSecure());
         Assert.assertEquals(cookie.isHttpOnly(), newCookie.isHttpOnly());
         Assert.assertEquals(cookie.getVersion(), newCookie.getVersion());
+    }
+
+    @Test
+    public void testSetName() {
+        SerializableHttpCookie cookie = new SerializableHttpCookie();
+        Assert.assertNull(cookie.getName());
+
+        cookie.setName("name");
+        Assert.assertEquals("name", cookie.getName());
+    }
+
+    @Test
+    public void testSetValue() {
+        SerializableHttpCookie cookie = new SerializableHttpCookie();
+        Assert.assertNull(cookie.getValue());
+
+        cookie.setValue("value");
+        Assert.assertEquals("value", cookie.getValue());
+    }
+
+    @Test
+    public void testSetComment() {
+        SerializableHttpCookie cookie = new SerializableHttpCookie();
+        Assert.assertNull(cookie.getComment());
+
+        cookie.setComment("comment");
+        Assert.assertEquals("comment", cookie.getComment());
+    }
+
+    @Test
+    public void testSetCommentURL() {
+        SerializableHttpCookie cookie = new SerializableHttpCookie();
+        Assert.assertNull(cookie.getCommentURL());
+
+        cookie.setCommentURL("commentURL");
+        Assert.assertEquals("commentURL", cookie.getCommentURL());
+    }
+
+    @Test
+    public void testSetDiscard() {
+        SerializableHttpCookie cookie = new SerializableHttpCookie();
+        Assert.assertFalse(cookie.getDiscard());
+
+        cookie.setDiscard(true);
+        Assert.assertEquals(true, cookie.getDiscard());
+    }
+
+    @Test
+    public void testSetMaxAge() {
+        SerializableHttpCookie cookie = new SerializableHttpCookie();
+        Assert.assertEquals(-1, cookie.getMaxAge());
+
+        cookie.setMaxAge(12345L);
+        Assert.assertEquals(12345L, cookie.getMaxAge());
     }
 }
